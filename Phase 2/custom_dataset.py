8d765631bb24d73a799c999a24c239a9341399dd
mohammadreza ahmaditeshnizi <mohammadrezaahmaditeshnizi@gmail.com>
2023-11-29 23:53:32 +0330

    small files added

---
+++ Phase 2/custom_dataset.py
@@ -0,0 +1,16 @@
+import torch
+
+
+# AttributeError: Can't get attribute 'CustomDataset' on <module '__main__' (built-in)>
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor([self.labels[idx]])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
